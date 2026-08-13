658c08b0c6defe9c70f001c3a357b0355713bcdb
Rohit-Dhade <rohitdhade99@gmail.com>
2026-08-13 16:11:20 +0530

    Fix MongoDB connection URL variable name

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ import userModel from "./models/user.model.js";
 
 const connectToMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
